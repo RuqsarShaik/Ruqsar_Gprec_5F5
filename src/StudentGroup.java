c5d5a230c41f38e85e933532364561ff024c94d2
RuqsarShaik <31990331+RuqsarShaik@users.noreply.github.com>
2017-09-16 12:32:38 +0530

    Add files via upload

--- src/StudentGroup.java
+++ src/StudentGroup.java
@@ -243,6 +243,19 @@ else if(students.index<0&&students.length>=students.index)
 	@Override
 	public void bubbleSort() {
 		// Add your implementation here
+	 int n = students.length;  
+        int temp = 0;  
+         for(int i=0; i < n; i++){  
+                 for(int j=1; j < (n-i); j++){  
+                          if(arr[j-1] > arr[j]){  
+                                 //swap elements  
+                                 temp = arr[j-1];  
+                                 arr[j-1] = arr[j];  
+                                 arr[j] = temp;  
+                         }  
+                          
+                 }  
+         }  
 	}
 
 	@Override
